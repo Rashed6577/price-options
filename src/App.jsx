5ec74ffa4436ff2ea1796d524c3804edf3f017c6
Rashed <rashedulhoqueceo@gmail.com>
2023-09-18 00:39:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-rose-500 font-bold text-center m-5'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
